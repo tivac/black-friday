28efaf977834104211cdcc608abf0c88f532dc31
Pat Cavit <pcavit@gmail.com>
2013-11-21 15:55:27 -0800

    Retries for purchasing made configurable
    Also improved output some

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ var fs      = require("fs"),
                 .argv,
     request = require("request"),
     async   = require("async"),
-    jar, _purchase;
-
-//console.log(argv);
+    jar, _purchase, _desc;
 
 // convert to MS
 argv.delay = argv.delay * 1000;
@@ -28,8 +26,6 @@ if(!argv._.length) {
 jar = request.jar();
 
 fs.readFileSync(argv.cookie, { encoding : "utf8" }).split("; ").forEach(function(cookie) {
-    console.log(cookie);
-    
     if(!cookie.length) {
         return;
     }
@@ -37,43 +33,48 @@ fs.readFileSync(argv.cookie, { encoding : "utf8" }).split("; ").forEach(function
     jar.add(request.cookie(cookie));
 });
 
+// describe a deal
+_desc = function(deal) {
+    return deal.dealID + " : " + deal.title;
+};
+
 // add item to cart
-_purchase = function(deal, details, done) {
-    request.post({
-        url : "http://www.amazon.com/gp/deal/ajax/redeemDeal.html/ref=gb1h_atc_c-3_0482_" + deal,
-        qs  : {
-            "marketplaceID" : argv.marketplace,
-            "dealID"        : details.legacyDealID,
-            "asin"          : details.teaserAsin || details.reviewAsin,
-            "sessionID"     : argv.session,
-            "nocache"       : Date.now()
-        },
-        json : true,
-        jar  : jar
-    }, function(err, http, body) {
-        debugger;
-        
-        if(err) {
-            console.error(err);
+_purchase = function(deal, done) {
+    // retry up to 5 times
+    async.timesSeries(argv.retries, function(n, cb) {
+        request.post({
+            url : "http://www.amazon.com/gp/deal/ajax/redeemDeal.html/ref=gb1h_atc_c-3_0482_" + deal.dealID,
+            qs  : {
+                "marketplaceID" : argv.marketplace,
+                "dealID"        : deal.legacyDealID,
+                "asin"          : deal.teaserAsin || deal.reviewAsin,
+                "sessionID"     : argv.session,
+                "nocache"       : Date.now()
+            },
+            jar  : jar,
+            json : true
+        }, function(err, http, body) {
+            if(err) {
+                console.error(err);
+                
+                return cb();
+            }
             
-            return done(err);
+            if(body.redeemed) {
+                console.log("Added deal: " + _desc(deal));
+                
+                // Bail early by sending back a fake error
+                return cb("success");
+            }
+            
+            cb();
+        });
+    }, function(err) {
+        if(err && err !== "success") {
+            console.log("Failed adding:" + _desc(deal));
         }
         
-        if(body.redeemed) {
-            console.log("Added deal: " + deal + " (" + details.title + ")");
-            
-            return done();
-        }
-        
-        if(details.retry) {
-            console.log("Failed adding:" + deal + " (" + details.title + ")");
-            
-            return done();
-        }
-        
-        details.retry = 1;
-        
-        _purchase(deal, details, done);
+        done();
     });
 };
 
@@ -91,7 +92,7 @@ request.post({
         
         "dealTargets" : argv._.map(function(id) {
             return {
-                dealID  : id,
+                dealID  : String(id),
                 itemIDs : null
             };
         }),
@@ -106,24 +107,33 @@ request.post({
             return;
         }
         
+        console.log("Deals:");
+        
+        Object.keys(body.dealStatus).forEach(function(deal) {
+            console.log(_desc(body.dealDetails[deal]));
+        });
+        
+        console.log("");
+        
+        console.log("Going to work...");
         async.each(
             Object.keys(body.dealStatus),
             function(deal, done) {
-                var details, delay;
+                var delay;
                 
-                details = body.dealDetails[deal];
-                delay   = body.dealStatus[deal].msToStart > argv.delay ?
-                            body.dealStatus[deal].msToStart - Math.floor(argv.delay / 2) :
+                deal  = body.dealDetails[deal];
+                delay = body.dealStatus[deal.dealID].msToStart > argv.delay ?
+                            body.dealStatus[deal.dealID].msToStart - Math.floor(argv.delay / 2) :
                             0;
                 
                 if(delay) {
-                    console.log("Waiting " + Math.floor(delay / 1000) + "s to add " + deal + " (" + details.title + ")");
+                    console.log(_desc(deal) + "\n  Waiting " + Math.floor(delay / 1000) + "s to add");
                 }
                 
                 setTimeout(
                     function() {
-                        console.log("Adding deal: " + deal + " (" + details.title + ")");
-                        _purchase(deal, details, done);
+                        console.log(_desc(deal) + " Added to cart!");
+                        _purchase(deal, done);
                     },
                     delay
                 );
